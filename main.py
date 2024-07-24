f2bb3f3f65971270096e5b4a5f5d0eae45416d97
souzatya <soujatya.sarkar2001@gmail.com>
2024-07-24 08:33:58 +0530

    updated main

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from dataloader import load_data
+from models.ESAU_net import ESAU
 
+# Get the dataloaders
 train_loader, val_loader, test_loader = load_data(train_dir='dataset/train', val_dir='dataset/val', test_dir='dataset/test', batch_size=8)
 
-# Example of iterating through the DataLoader
-for images, masks in train_loader:
-    print(images.shape, masks.shape)
-    # Do something with the images and labels
+# Model Initialization
+model = ESAU()
 
-# Example of iterating through the DataLoader
-for images, masks in val_loader:
-    print(images.shape, masks.shape)
-    # Do something with the images and labels
-
-# Example of iterating through the DataLoader
-for images, masks in test_loader:
-    print(images.shape, masks.shape)
-    # Do something with the images and labels
+# Train the model
+train_model(model, train_loader, val_loader)
