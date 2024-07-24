79236df93c1fd31ae6bbbdc1f12a88a2daf8734d
souzatya <soujatya.sarkar2001@gmail.com>
2024-07-24 08:34:35 +0530

    updated main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dataloader import load_data
 from models.ESAU_net import ESAU
+from train import train_model
 
 # Get the dataloaders
 train_loader, val_loader, test_loader = load_data(train_dir='dataset/train', val_dir='dataset/val', test_dir='dataset/test', batch_size=8)
